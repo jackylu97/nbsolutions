1a2758aa4f172abc83347129b25c1188a0f43bf8
Jacky Lu <jackylu@berkeley.edu>
2017-07-24 08:51:35 -0700

    Added LICENSE and updated setup.py for upload to pypi

--- setup.py
+++ setup.py
@@ -4,7 +4,8 @@ setuptools.setup(
     name="nbsolutions",
     version='0.1.0',
     author="Jacky Lu",
-    description="",
+    url="https://github.com/jackylu97/nbsolutions"
+    description="Simple Jupyter extension that allows the user to mark cells as 'solution' cells not meant to be viewed by students.",
     packages= ["nbsolutions"],
     install_requires=[
         'notebook',
